87f5cb8352840b8a19793d8ebf0e3d3195570f4e
rajranjan672 <raj99391435@gmail.com>
2023-09-20 10:58:49 +0530

    middleware
    addgv

--- routes/User.js
+++ routes/User.js
@@ -65,7 +65,26 @@ router.post('/createuser',[
 
 })
 
+const verifyToken = (req, res, next) => {
+    var token =
+      req.body.token || req.query.token || req.headers["x-access-token"];
+  
+    if (!token) {
+      return res.status(403).send("A token is required for authentication");
+    }
+    try {
+      const decoded = jwt.verify(token, config.JWT_SECRET);
+      req.user = decoded;
+    } catch (err) {
+      return res.status(401).send("Invalid Token");
+    }
+    return next();
+  };
 
+  router.get('/get', verifyToken, async(req, res) => {
+    const ress = await User.find()
+    res.send(ress)
+})
 
 
 //  user login
