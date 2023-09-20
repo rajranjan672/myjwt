df9d25e4939a719b00e437e3e0940b9bdc685c75
rajranjan672 <raj99391435@gmail.com>
2023-09-20 11:56:41 +0530

    jwt auth

--- routes/User.js
+++ routes/User.js
@@ -73,7 +73,7 @@ const verifyToken = (req, res, next) => {
       return res.status(403).send("A token is required for authentication");
     }
     try {
-      const decoded = jwt.verify(token, config.JWT_SECRET);
+      const decoded = jwt.verify(token, JWT_SECRET);
       req.user = decoded;
     } catch (err) {
       return res.status(401).send("Invalid Token");
